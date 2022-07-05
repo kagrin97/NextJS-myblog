d25b4adc27e957e9171db35427aeec126259df22
MinGyu-Kang <kagrin97@gmail.com>
2022-07-05 17:49:54 +0900

    패키지 포스터작성

--- data/navlinks.ts
+++ data/navlinks.ts
@@ -2,6 +2,7 @@ const navlinks: { title: string; link: string }[] = [
   { title: "JavaScript", link: "/js" },
   { title: "TypeScript", link: "/type-script" },
   { title: "NextJS", link: "/next-js" },
+  { title: "Other", link: "/other" },
 ];
 
 export default navlinks;
