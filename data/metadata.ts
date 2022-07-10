9a1c25cb6f140c4bbcc789ead29cc1510e813150
MinGyu-Kang <kagrin97@gmail.com>
2022-07-10 19:42:28 +0900

    메타데이터 설명 수정

--- data/metadata.ts
+++ data/metadata.ts
@@ -1,7 +1,7 @@
 const metadata = {
   type: "blog",
   title: "Kang's blog",
-  description: "kagrin97",
+  description: "개발자로서의 개발을 위한 개발생활",
   author: "kagrin97",
   url: "https://kagrin97-blog.vercel.app/",
 };
