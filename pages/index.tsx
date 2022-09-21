32b3a9bda4922807e295af89a0209aea34fd0deb
MinGyu-Kang <kagrin97@gmail.com>
2022-09-21 15:02:08 +0900

    [#6] feat : home페이지를 간단한 자기소개로 작성했습니다.

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,18 +23,32 @@ const Home = () => {
             <span className={`ml-3 mb-3`}>MinGyu-Kang Development Blog</span>
             <span className={`ml-3`}>개발을 위한 개발생활</span>
           </article>
-
-          <div className={`absolute bottom-5 left-5 animate-pulse`}>
-            <Link href="https://github.com/kagrin97">
-              <Image
-                src={`/github.png`}
-                alt="깃허브"
-                width={40}
-                height={40}
-                className={`rounded-3xl hover:cursor-pointer`}
-              />
-            </Link>
-          </div>
+        </section>
+        <div className={`absolute bottom-5 left-5 animate-pulse`}></div>
+        <section className="mt-10 text-center">
+          <Image
+            src={`/아바타.jpg`}
+            alt="깃허브"
+            width={100}
+            height={100}
+            className={`rounded-full`}
+          />
+          <Link href="https://github.com/kagrin97">
+            <Image
+              src={`/github.png`}
+              alt="깃허브"
+              width={40}
+              height={40}
+              className={`rounded-3xl hover:cursor-pointer`}
+            />
+          </Link>
+          <h1 className="text-2xl my-10">언제나 공부중인 민규</h1>
+          <h2 className="text-xl">
+            저는 다른 개발자들의 prototype이 되는것이 꿈입니다.
+          </h2>
+          <p className="my-10 opacity-50">
+            GitHub버튼을 클릭하면 제 GitHub으로 이동됩니다!
+          </p>
         </section>
       </div>
     </Container>
