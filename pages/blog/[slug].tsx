984d871ad0712c74e2dcaf5e736367625d423705
MinGyu-Kang <kagrin97@gmail.com>
2022-09-21 11:36:24 +0900

    feat : 각각포스터밑에 만들어진 년월일을 추가했습니다.

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -16,7 +16,11 @@ const Post = ({ post }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <Container customMeta={customMeta} className="relative">
       <article className="mt-10 prose max-w-none">
-        <h1 className="text-green-500">{post.title}</h1>
+        <h1 className="text-green-500 text-center">{post.title}</h1>
+        <h3 className="text-green-600 text-end italic">
+          {post.date.slice(0, 4)}년 {post.date.slice(5, 7)}월
+          {post.date.slice(8, 10)}일
+        </h3>
         <article className={`w-full dark:text-slate-50 text-neutral-900`}>
           <MDXComponent />
         </article>
