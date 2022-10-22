b9447e4ced65b8501dc3999fd86afdee595a23cb
MinGyu-Kang <kagrin97@gmail.com>
2022-10-22 23:20:37 +0900

    [#23] fix : _app.tsx 파일에 조건문을 걸어서 React Hydration Error 해결

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,25 @@
 import "styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import { useEffect, useState } from "react";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+  const [showChild, setShowChild] = useState(false);
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
+
+  if (!showChild) {
+    return null;
+  }
+
+  if (typeof window === "undefined") {
+    return <></>;
+  } else {
+    return (
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    );
+  }
 }
