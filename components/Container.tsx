cf1a51faf49020a0bb18118e59e860bf343b9f9e
MinGyu-Kang <kagrin97@gmail.com>
2022-10-24 14:45:28 +0900

    [#17] fix : 기존에 metatag를 컴포넌트로 빼서 분리시켰더니 google 크롤링이 제대로 되지 않아서 다시 합쳤습니다

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Image from "next/image";
+import Head from "next/head";
 import Link from "next/link";
 
 import metadata from "data/metadata";
 import Nav from "components/Nav";
-import MetaTag from "components/MetaTag";
 
 import NextProgress from "next-progress";
 import ProgressBar from "react-scroll-progress-bar";
@@ -21,7 +21,52 @@ export default function Container(props) {
 
   return (
     <main className={`w-full flex flex-col items-center p-3 relative`}>
-      <MetaTag meta={meta} />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>{meta.title}</title>
+
+        <meta name="Date" content={meta.date} />
+        <meta name="description" content={meta.description} />
+        <meta name="by" content={meta.author} />
+
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:article:author" content={meta.author} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:site_name" content={meta.author} />
+        <meta property="og:url" content={meta.url} />
+
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+
+        <meta property="dg:plink" content={meta.url} />
+        <meta name="plink" content={meta.url} />
+        <meta name="title" content={meta.title} />
+        <meta name="article:media_name" content={meta.author} />
+        <meta property="article:mobile_url" content={meta.url} />
+        <meta property="article:pc_url" content={meta.url} />
+        <meta property="article:mobile_view_url" content={meta.url} />
+        <meta property="article:pc_view_url" content={meta.url} />
+        <meta property="article:talk_channel_view_url" content={meta.url} />
+        <meta
+          property="article:pc_service_home"
+          content="https://kagrin97-blog.vercel.app"
+        />
+        <meta
+          property="article:mobile_service_home"
+          content="https://kagrin97-blog.vercel.app/m"
+        />
+        <meta property="article:published_time" content={meta.date} />
+
+        <meta
+          name="naver-site-verification"
+          content="da98b9d3244abed109afb03767f0adf1a713f595"
+        />
+        <meta
+          name="google-site-verification"
+          content="tHzgblVRtM5UJi1BBTUouxZXp_txiBpb0KwUytGz0rI"
+        />
+      </Head>
       <NextProgress
         color="#22c55e"
         delay={300}
