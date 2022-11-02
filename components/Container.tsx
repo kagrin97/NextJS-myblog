9693d327d2a8170a64cd25fc1adc329b6b3d014c
MinGyu-Kang <kagrin97@gmail.com>
2022-11-02 18:46:16 +0900

    fix : 구글 서치콘솔이 meta tag를 읽지 못해서 소유권을 확인하지 못하는 경우에 따라서 필요한 metatag를 제외한 다른 태그를  삭제했습니다

--- components/Container.tsx
+++ components/Container.tsx
@@ -24,10 +24,6 @@ export default function Container(props) {
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <title>{meta.title}</title>
-        <meta
-          name="google-site-verification"
-          content="tHzgblVRtM5UJi1BBTUouxZXp_txiBpb0KwUytGz0rI"
-        />
 
         <meta name="Date" content={meta.date} />
         <meta name="description" content={meta.description} />
@@ -39,32 +35,15 @@ export default function Container(props) {
         <meta property="og:description" content={meta.description} />
         <meta property="og:site_name" content={meta.author} />
         <meta property="og:url" content={meta.url} />
-
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
-
-        <meta property="dg:plink" content={meta.url} />
-        <meta name="plink" content={meta.url} />
-        <meta name="title" content={meta.title} />
-        <meta name="article:media_name" content={meta.author} />
-        <meta property="article:mobile_url" content={meta.url} />
-        <meta property="article:pc_url" content={meta.url} />
-        <meta property="article:mobile_view_url" content={meta.url} />
-        <meta property="article:pc_view_url" content={meta.url} />
-        <meta property="article:talk_channel_view_url" content={meta.url} />
-        <meta
-          property="article:pc_service_home"
-          content="https://kagrin97-blog.vercel.app"
-        />
-        <meta
-          property="article:mobile_service_home"
-          content="https://kagrin97-blog.vercel.app/m"
-        />
         <meta property="article:published_time" content={meta.date} />
 
         <meta
           name="naver-site-verification"
           content="da98b9d3244abed109afb03767f0adf1a713f595"
+        />
+        <meta
+          name="google-site-verification"
+          content="tHzgblVRtM5UJi1BBTUouxZXp_txiBpb0KwUytGz0rI"
         />
       </Head>
       <NextProgress
