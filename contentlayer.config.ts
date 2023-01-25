aec8c69c2706099b9510b67502dfe9623db65af4
MinGyu-Kang <kagrin97@gmail.com>
2023-01-25 15:12:23 +0900

    refactor : defineDocumentType로 document를 생성하는 로직을 생성해 기존에 몇백줄 되는 코드를 하나의 함수로 refoctoring 했음

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -3,148 +3,33 @@ import { defineDocumentType, makeSource } from "contentlayer/source-files";
 import rehypePrism from "rehype-prism-plus";
 import rehypeCodeTitles from "rehype-code-titles";
 
-export const Js = defineDocumentType(() => ({
-  name: "Js",
-  contentType: "mdx",
-  filePathPattern: `js/*.mdx`,
-  fields: {
-    title: { type: "string", required: true },
-    date: { type: "string", required: true },
-    description: { type: "string", required: true },
-    category: { type: "string", required: true },
-  },
-  computedFields: {
-    slug: {
-      type: "string",
-      resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, ""),
+const configDocumentType = (docName: string, filePath: string) => {
+  return defineDocumentType(() => ({
+    name: docName,
+    contentType: "mdx",
+    filePathPattern: `${filePath}/*.mdx`,
+    fields: {
+      title: { type: "string", required: true },
+      date: { type: "string", required: true },
+      description: { type: "string", required: true },
+      category: { type: "string", required: true },
     },
-  },
-}));
-
-export const Git = defineDocumentType(() => ({
-  name: "Git",
-  contentType: "mdx",
-  filePathPattern: `git/*.mdx`,
-  fields: {
-    title: { type: "string", required: true },
-    date: { type: "string", required: true },
-    description: { type: "string", required: true },
-    category: { type: "string", required: true },
-  },
-  computedFields: {
-    slug: {
-      type: "string",
-      resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, ""),
+    computedFields: {
+      slug: {
+        type: "string",
+        resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, ""),
+      },
     },
-  },
-}));
-export const Next = defineDocumentType(() => ({
-  name: "Next",
-  contentType: "mdx",
-  filePathPattern: `next/*.mdx`,
-  fields: {
-    title: { type: "string", required: true },
-    date: { type: "string", required: true },
-    description: { type: "string", required: true },
-    category: { type: "string", required: true },
-  },
-  computedFields: {
-    slug: {
-      type: "string",
-      resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, ""),
-    },
-  },
-}));
-
-export const Other = defineDocumentType(() => ({
-  name: "Other",
-  contentType: "mdx",
-  filePathPattern: `other/*.mdx`,
-  fields: {
-    title: { type: "string", required: true },
-    date: { type: "string", required: true },
-    description: { type: "string", required: true },
-    category: { type: "string", required: true },
-  },
-  computedFields: {
-    slug: {
-      type: "string",
-      resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, ""),
-    },
-  },
-}));
-
-export const React = defineDocumentType(() => ({
-  name: "React",
-  contentType: "mdx",
-  filePathPattern: `react/*.mdx`,
-  fields: {
-    title: { type: "string", required: true },
-    date: { type: "string", required: true },
-    description: { type: "string", required: true },
-    category: { type: "string", required: true },
-  },
-  computedFields: {
-    slug: {
-      type: "string",
-      resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, ""),
-    },
-  },
-}));
-
-export const Types = defineDocumentType(() => ({
-  name: "Types",
-  contentType: "mdx",
-  filePathPattern: `types/*.mdx`,
-  fields: {
-    title: { type: "string", required: true },
-    date: { type: "string", required: true },
-    description: { type: "string", required: true },
-    category: { type: "string", required: true },
-  },
-  computedFields: {
-    slug: {
-      type: "string",
-      resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, ""),
-    },
-  },
-}));
-
-export const Algorithm = defineDocumentType(() => ({
-  name: "Algorithm",
-  contentType: "mdx",
-  filePathPattern: `algorithm/*.mdx`,
-  fields: {
-    title: { type: "string", required: true },
-    date: { type: "string", required: true },
-    description: { type: "string", required: true },
-    category: { type: "string", required: true },
-  },
-  computedFields: {
-    slug: {
-      type: "string",
-      resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, ""),
-    },
-  },
-}));
-
-export const DataBase = defineDocumentType(() => ({
-  name: "DataBase",
-  contentType: "mdx",
-  filePathPattern: `db/*.mdx`,
-  fields: {
-    title: { type: "string", required: true },
-    date: { type: "string", required: true },
-    description: { type: "string", required: true },
-    category: { type: "string", required: true },
-  },
-  computedFields: {
-    slug: {
-      type: "string",
-      resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, ""),
-    },
-  },
-}));
+  }));
+};
+const Js = configDocumentType("Js", "js");
+const Git = configDocumentType("Git", "git");
+const Next = configDocumentType("Next", "next");
+const Other = configDocumentType("Other", "other");
+const React = configDocumentType("React", "react");
+const Types = configDocumentType("Types", "types");
+const Algorithm = configDocumentType("Algorithm", "algorithm");
+const DataBase = configDocumentType("DataBase", "db");
 
 export default makeSource({
   contentDirPath: "posts",
