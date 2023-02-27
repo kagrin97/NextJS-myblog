cb2c67f02b86f67155e814099bc26908e4c7bb20
MinGyu-Kang <kagrin97@gmail.com>
2023-02-27 19:17:36 +0900

    style : SeachBar가 599px아래이면 width값을 3/5 => 95%로 변경하는 코드 작성

--- components/SeachBar.tsx
+++ components/SeachBar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function SeachBar({ searchTitle, onChangeSearchTitle }) {
   return (
-    <form className={`fixed w-4/5 max-w-2xl flex mt-4`}>
+    <form className={`fixed w-3/5 max-w-2xl flex mt-4`}>
       <input
         type="text"
         value={searchTitle}
@@ -10,6 +10,13 @@ export default function SeachBar({ searchTitle, onChangeSearchTitle }) {
         className={`w-full px-3 py-5 rounded-lg bg-gray-200 dark:bg-gray-600 mt-3 focus:outline-none`}
         placeholder="포스터의 제목을 입력하세요. 😎"
       />
+      <style jsx>{`
+        @media only screen and (max-width: 599px) {
+          form {
+            width: 95%;
+          }
+        }
+      `}</style>
     </form>
   );
 }
