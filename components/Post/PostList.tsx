c9f85855f2bab45981e70e335b0b3c9f774755ed
MinGyu-Kang <kagrin97@gmail.com>
2023-03-09 15:32:50 +0900

    reafactor : PostList 중복 제거, 타입을 정합했습니다.

--- components/Post/PostList.tsx
+++ components/Post/PostList.tsx
@@ -1,35 +1,35 @@
 import React from "react";
-
 import BlogPost from "./BlogPost";
 
-export default function PostList({ searchPosts, posts }) {
+interface Post {
+  date: string;
+  title: string;
+  description: string;
+  _raw: {
+    flattenedPath: string;
+  };
+  _id: string;
+}
+
+interface Props {
+  searchPosts: Post[];
+  posts: Post[];
+}
+
+export default function PostList({ searchPosts, posts }: Props): Element {
+  const postsToRender = searchPosts.length ? searchPosts : posts;
+
   return (
-    <article className={`mt-20 flex flex-col`}>
-      {searchPosts.length ? (
-        <div>
-          {searchPosts.map((post) => (
-            <BlogPost
-              date={post.date.slice(0, 10)}
-              title={post.title}
-              des={post.description}
-              slug={post._raw.flattenedPath}
-              key={post._id}
-            />
-          ))}
-        </div>
-      ) : (
-        <div>
-          {posts.map((post) => (
-            <BlogPost
-              date={post.date.slice(0, 10)}
-              title={post.title}
-              des={post.description}
-              slug={post._raw.flattenedPath}
-              key={post._id}
-            />
-          ))}
-        </div>
-      )}
+    <article className="mt-20 flex flex-col">
+      {postsToRender.map((post) => (
+        <BlogPost
+          date={post.date.slice(0, 10)}
+          title={post.title}
+          des={post.description}
+          slug={post._raw.flattenedPath}
+          key={post._id}
+        />
+      ))}
     </article>
   );
 }
