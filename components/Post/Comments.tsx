3885f6bdb921eabc8084c3015bb2998dc7be264b
MinGyu-Kang <kagrin97@gmail.com>
2023-03-09 13:33:11 +0900

    feat : utterances를 이용해 만든 comments를 dark, light 모드에 따라 theme을 변경하도록 구현

--- components/Post/Comments.tsx
+++ components/Post/Comments.tsx
@@ -1,19 +1,36 @@
 import React, { useEffect, useRef } from "react";
+import { useTheme } from "next-themes";
 
 export default function Comments() {
   const ref = useRef(null);
+  const { theme } = useTheme();
 
-  useEffect(() => {
+  const makeComments = () => {
     const script = document.createElement("script");
 
     script.src = "https://utteranc.es/client.js";
-    script.async = true;
+    script.async = false;
     script.setAttribute("repo", "kagrin97/NextJS-myblog");
     script.setAttribute("issue-term", "title");
-    script.setAttribute("theme", "github-light");
+    if (theme === "dark") {
+      script.setAttribute("theme", "dark-blue");
+    } else {
+      script.setAttribute("theme", "github-light");
+    }
     script.setAttribute("label", "blog-comment");
     ref.current.appendChild(script);
-  }, []);
+  };
+
+  const removeExistedComments = () => {
+    const existingScript = ref.current.querySelector(".utterances");
+    if (existingScript) {
+      existingScript.remove();
+    }
+  };
+  useEffect(() => {
+    makeComments();
+    removeExistedComments();
+  }, [theme]);
 
   return <div ref={ref}></div>;
 }
