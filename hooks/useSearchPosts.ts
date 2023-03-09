ecff313f7de5383659e2960ccdbe8bceb5748829
MinGyu-Kang <kagrin97@gmail.com>
2023-03-09 16:22:13 +0900

    refactor : useSearchPosts hook 타입 정합 및 리팩토링

--- hooks/useSearchPosts.ts
+++ hooks/useSearchPosts.ts
@@ -1,26 +1,32 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import { Post } from "types/posts";
 
-export default function useSearchPosts(allPosts) {
-  const [searchTitle, setSearchTitle] = useState("");
-  const [searchPosts, setSearchPosts] = useState([]);
+type SearchPostsResult = {
+  searchTitle: string;
+  searchPosts: Post[];
+  onChangeSearchTitle: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  getSearchPosts: () => void;
+};
+
+export default function useSearchPosts(allPosts: Post[]): SearchPostsResult {
+  const [searchTitle, setSearchTitle] = useState<string>("");
+  const [searchPosts, setSearchPosts] = useState<Post[]>([]);
 
   const onChangeSearchTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTitle(e.target.value);
   };
 
   const getSearchPosts = () => {
+    const tmp: Post[] = [];
     if (searchTitle !== "") {
-      const tmp = [];
-      allPosts.map((post) => {
-        const regex = new RegExp(searchTitle, "gim");
+      const regex = new RegExp(searchTitle, "gim");
+      allPosts.forEach((post: Post) => {
         if (regex.test(post.title)) {
           tmp.push(post);
         }
       });
-      setSearchPosts(tmp);
-    } else {
-      setSearchPosts([]);
     }
+    setSearchPosts(tmp);
   };
 
   return { searchTitle, searchPosts, onChangeSearchTitle, getSearchPosts };
