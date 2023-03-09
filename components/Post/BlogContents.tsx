da8767d82a437b93025ce1ae8cd58d0f4fb48614
MinGyu-Kang <kagrin97@gmail.com>
2023-03-09 15:12:08 +0900

    refactor : BlogContents 컴포넌트를 리팩터링하고 중복 코드를 제거 및 타입을 정합 했습니다.

--- components/Post/BlogContents.tsx
+++ components/Post/BlogContents.tsx
@@ -1,71 +1,73 @@
 import React, { useEffect } from "react";
 import ReactDOMServer from "react-dom/server";
-
 import axios from "axios";
-
 import cheerio from "cheerio";
 import OpenGraphPreview from "../UIElements/OpenGraphPreview";
 
 export default function BlogContents({ post, MDXComponent }) {
-  const handlerOpenGraphPreview = () => {
-    const $linkList = document.querySelectorAll("#link");
-    $linkList.forEach(async (el: any, index) => {
-      try {
-        const urlPath = el.href;
-        const response = await axios.get(
-          `/api/proxy?url=${encodeURIComponent(urlPath)}`
-        );
-        const html = response.data;
-        const $ = cheerio.load(html);
-        const ogTitle =
-          $('meta[property="og:title"]').attr("content") || $("title").text();
-        const ogDescription = $('meta[property="og:description"]').attr(
-          "content"
-        );
-        const ogImage = $('meta[property="og:image"]').attr("content");
-        let aHtml: string = "";
-
-        aHtml = ReactDOMServer.renderToString(
-          <OpenGraphPreview
-            urlPath={urlPath}
-            ogTitle={ogTitle}
-            ogDescription={ogDescription}
-            ogImage={ogImage}
-          />
-        );
-
-        el.outerHTML = aHtml;
-      } catch (err) {
-        console.log(err);
-      }
-    });
+  const handleLinkPreview = async (el: HTMLAnchorElement) => {
+    try {
+      const urlPath = el.href;
+      const response = await axios.get(
+        `/api/proxy?url=${encodeURIComponent(urlPath)}`
+      );
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const ogTitle =
+        $('meta[property="og:title"]').attr("content") || $("title").text();
+      const ogDescription = $('meta[property="og:description"]').attr(
+        "content"
+      );
+      const ogImage = $('meta[property="og:image"]').attr("content");
+      const aHtml = ReactDOMServer.renderToString(
+        <OpenGraphPreview
+          urlPath={urlPath}
+          ogTitle={ogTitle}
+          ogDescription={ogDescription}
+          ogImage={ogImage}
+        />
+      );
+      el.outerHTML = aHtml;
+    } catch (err) {
+      console.log(err);
+    }
   };
 
-  const handlerChapterScroll = () => {
-    const links = document.querySelectorAll('a[href^="#"]');
-
-    links.forEach((link) => {
-      link.addEventListener("click", function (event) {
-        event.preventDefault();
-        const linkId = link.getAttribute("href")?.slice(1);
-        if (linkId) {
-          const target = document.getElementById(linkId);
-          if (target) {
-            target.scrollIntoView({
-              behavior: "smooth",
-            });
-          }
-        }
-      });
-    });
+  const handleLinkClick = (
+    event: React.MouseEvent<HTMLAnchorElement>,
+    link: HTMLAnchorElement
+  ) => {
+    event.preventDefault();
+    const linkId = link.getAttribute("href")?.slice(1);
+    if (linkId) {
+      const target = document.getElementById(linkId);
+      if (target) {
+        target.scrollIntoView({ behavior: "smooth" });
+      }
+    }
   };
 
   useEffect(() => {
     if (MDXComponent) {
-      handlerOpenGraphPreview();
-      handlerChapterScroll();
+      const linkList = document.querySelectorAll("#link");
+      linkList.forEach((el: Element) => {
+        if (el instanceof HTMLAnchorElement) {
+          handleLinkPreview(el);
+        }
+      });
+      const links = document.querySelectorAll('a[href^="#"]');
+      links.forEach((link) => {
+        if (link instanceof HTMLAnchorElement) {
+          link.addEventListener(
+            "click",
+            (event: React.MouseEvent<HTMLAnchorElement>) =>
+              handleLinkClick(event, link)
+          );
+        }
+      });
     }
-  }, []);
+  }, [MDXComponent]);
+
   return (
     <article className="mt-10 prose max-w-none">
       <h1 className="text-green-500 text-center maxSm:text-2xl">
@@ -75,7 +77,7 @@ export default function BlogContents({ post, MDXComponent }) {
         {post.date.slice(0, 4)}년 {post.date.slice(5, 7)}월
         {post.date.slice(8, 10)}일
       </h3>
-      <article className={`w-full dark:text-slate-50 text-neutral-900`}>
+      <article className="w-full dark:text-slate-50 text-neutral-900">
         <MDXComponent />
       </article>
     </article>
